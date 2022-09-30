77397e130c87875316dfcdd3920c10e55b3d7642
JapmannKaur <88281936+JapmannKaur@users.noreply.github.com>
2022-10-01 03:42:23 +0530

    Add files via upload

---
+++ Arrays/traversal.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+        cin>>arr[n];
+    for(int i=0;i<n;i++)
+        cout<<arr[i];
+}
